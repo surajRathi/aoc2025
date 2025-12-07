aa72513dc83d103a420346fa7b0437baecf74b95
Suraj <suraj.rathi00@gmail.com>
2025-12-07 02:03:52 -0500

    part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::iter::repeat_n;
 
 fn read_file() -> Vec<String> {
     let path = std::path::Path::new("./data/day2.txt");
@@ -24,6 +25,7 @@ struct Range {
     max: u128,
 }
 fn parse_range(line: &str) -> Range {
+    // TODO: Use split_once
     let (first, second_minus) = line.split_at(line.find('-').unwrap());
     let second = &second_minus[1..];
 
@@ -51,6 +53,45 @@ pub fn part1() {
             let (first, second) = s.split_at(s.len() / 2);
             if first == second {
                 sum += code;
+            }
+        }
+    }
+
+    println!("Sum: {}", sum);
+    return;
+}
+
+// TODO: Understand - this is copied code.
+pub fn div_rem<T: std::ops::Div<Output = T> + std::ops::Rem<Output = T> + Copy>(
+    x: T,
+    y: T,
+) -> (T, T) {
+    let quot = x / y;
+    let rem = x % y;
+    (quot, rem)
+}
+#[allow(dead_code)]
+pub fn part2() {
+    let mut sum: u128 = 0;
+
+    for range in read_file().into_iter().map(|l| parse_range(&l)) {
+        for code in range.min..=range.max {
+            let s = code.to_string();
+            for i in 1..=(s.len() / 2) {
+                let (quotient, rem) = div_rem(s.len(), i);
+                if rem != 0 {
+                    continue;
+                }
+
+                let rep = s.chars().into_iter().take(i).collect::<String>();
+                let s2 = repeat_n(rep, quotient).collect::<String>();
+
+                if (s == s2) {
+                    println!("{}", code);
+                    sum += code;
+                    break;
+
+                }
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod day2;
 
 fn main() {
-    day2::part1();
+    day2::part2();
 }
