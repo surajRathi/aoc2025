7f16bc395a15b21b18f959a70ffd4cec80d33b4c
Suraj <suraj.rathi00@gmail.com>
2025-12-16 06:03:27 +0530

    solve

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,15 @@
-use std::fmt::Formatter;
 use std::io;
 use std::io::Read;
+use std::ops::{Add, Mul};
+use std::slice::Iter;
 
-fn read_file() -> Result<usize, io::Error> {
+type OperationFn = fn(u64, u64) -> u64;
+
+struct OperationData {
+    func: OperationFn,
+    default: u64,
+}
+fn do_problem() -> Result<u64, io::Error> {
     let path = std::path::Path::new("./data/day6.txt");
     println!("Reading {}", path.display());
 
@@ -10,45 +17,67 @@ fn read_file() -> Result<usize, io::Error> {
     let mut file = std::fs::File::open(&path)?;
     let mut s = String::new();
     let _ = file.read_to_string(&mut s)?;
+    s = s.trim().to_string();
 
     let mut lines = s.lines().into_iter();
 
-    // if lines.count() != 4 {
-    //     return Err(io::Error::new(
-    //         std::io::ErrorKind::InvalidData,
-    //         "File had {lines.count()} lines",
-    //     ));
-    // }
-
     let x = lines
         .by_ref()
-        .take(3)
+        .take(4)
         .map(|line| {
-            line.split("\\S+")
-                .map(|s| s.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()
+            line.split_whitespace()
+                .map(|s| {
+                    s.parse::<u64>()
+                        .expect(format!("Failed to parse number {s} in line {line}").as_str())
+                })
+                .collect::<Vec<u64>>()
         })
-        .collect::<Vec<Vec<u32>>>();
+        .collect::<Vec<Vec<u64>>>();
+
+    type OperationFn = fn(u64, u64) -> u64;
 
     let y = lines
-        .skip(3)
-        .take(1)
         .next()
         .unwrap()
-        .split("\\S+")
+        .split_whitespace()
         .map(|ch| match ch {
-            "+" => std::ops::Add::add,
-            "*" => std::ops::Mul::mul,
+            "+" => OperationData {
+                func: (u64::add as OperationFn),
+                default: 0,
+            },
+            "*" => OperationData {
+                func: u64::mul as OperationFn,
+                default: 1,
+            },
             _ => panic!("Unknown operation type {ch}"),
         })
-        .collect();
+        .collect::<Vec<OperationData>>();
 
-    Ok(3usize)
+    assert!(x.iter().all(|x| x.len() == y.len()));
+
+    let mut sum = 0;
+    for (op, args) in std::iter::zip(y, zip::<u64>(&x)) {
+        let res = args
+            .iter()
+            .fold(op.default, |acc: u64, val| (op.func)(acc, **val));
+        sum += res;
+    }
+    Ok(sum)
+}
+
+fn zip<T>(vector_vectors: &Vec<Vec<T>>) -> impl Iterator<Item = Vec<&T>> {
+    assert!(!vector_vectors.is_empty());
+    let len = vector_vectors[0].len();
+    assert!(vector_vectors.iter().by_ref().all(|v| v.len() == len));
+
+    let mut iters: Vec<Iter<T>> = vector_vectors.iter().map(|v| v.iter()).collect();
+
+    (0..len).map(move |_| iters.iter_mut().map(|v| v.next().unwrap()).collect())
 }
 
 #[allow(dead_code)]
 pub fn part1() {
-    let lines = read_file();
-
+    let res = do_problem();
+    println!("Sum: {}", res.unwrap());
     return;
 }
