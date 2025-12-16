9bc2317400f2fc588fdeafd741f60c4e86465e43
Suraj <suraj.rathi00@gmail.com>
2025-12-16 08:41:19 +0530

    part2 done

--- src/day6.rs
+++ src/day6.rs
@@ -10,19 +10,40 @@ struct OperationData {
     default: u64,
 }
 
-fn parse_op_data(line: &str) -> Vec<OperationData> {
+fn parse_op_data(val: &str) -> OperationData {
+    match val.trim() {
+        "+" => OperationData {
+            func: u64::add as OperationFn,
+            default: 0,
+        },
+        "*" => OperationData {
+            func: u64::mul as OperationFn,
+            default: 1,
+        },
+        _ => panic!("Unknown operation type {val}"),
+    }
+}
+
+fn try_parse_op_data(val: &str) -> Option<OperationData> {
+    // if !val.trim().is_empty() {
+    //     println!("Parsing {val}");
+    // }
+    match val.trim() {
+        "+" => Some(OperationData {
+            func: u64::add as OperationFn,
+            default: 0,
+        }),
+        "*" => Some(OperationData {
+            func: u64::mul as OperationFn,
+            default: 1,
+        }),
+        _ => None,
+    }
+}
+
+fn parse_op_data_vec(line: &str) -> Vec<OperationData> {
     line.split_whitespace()
-        .map(|ch| match ch {
-            "+" => OperationData {
-                func: (u64::add as OperationFn),
-                default: 0,
-            },
-            "*" => OperationData {
-                func: u64::mul as OperationFn,
-                default: 1,
-            },
-            _ => panic!("Unknown operation type {ch}"),
-        })
+        .map(parse_op_data)
         .collect::<Vec<OperationData>>()
 }
 fn do_problem() -> Result<u64, io::Error> {
@@ -50,9 +71,7 @@ fn do_problem() -> Result<u64, io::Error> {
         })
         .collect::<Vec<Vec<u64>>>();
 
-    type OperationFn = fn(u64, u64) -> u64;
-
-    let y = parse_op_data(lines.next().unwrap());
+    let y = parse_op_data_vec(lines.next().unwrap());
 
     assert!(x.iter().all(|x| x.len() == y.len()));
 
@@ -66,6 +85,11 @@ fn do_problem() -> Result<u64, io::Error> {
     Ok(sum)
 }
 
+struct Segment {
+    start_index: usize,
+    operation: OperationData,
+    operands: Vec<u64>,
+}
 fn do_problem_2() -> Result<u64, io::Error> {
     let path = std::path::Path::new("./data/day6.txt");
     println!("Reading {}", path.display());
@@ -78,39 +102,63 @@ fn do_problem_2() -> Result<u64, io::Error> {
 
     let mut lines = s.lines().into_iter();
 
-    let x = lines
+    let number_lines: Vec<&str> = lines.by_ref().take(4).collect();
+
+    let mut segments: Vec<Segment> = lines
         .by_ref()
-        .take(4)
-        .map(|line| {
-            line.split_whitespace()
-                .map(|s| s.trim())
-                .collect::<Vec<&str>>()
+        .next()
+        .unwrap()
+        .bytes()
+        .enumerate()
+        .filter_map(|(i, b)| {
+            let data = try_parse_op_data((b as char).to_string().as_str());
+            match data {
+                None => None,
+                Some(data) => Some(Segment {
+                    start_index: i,
+                    operation: data,
+                    operands: vec![],
+                }),
+            }
         })
-        .collect::<Vec<Vec<&str>>>();
+        .collect();
 
-    type OperationFn = fn(u64, u64) -> u64;
+    let mut last: Option<usize> = None;
 
-    let y = parse_op_data(lines.next().unwrap());
+    for data in segments.iter_mut().rev() {
+        let slice: Vec<String> = number_lines
+            .iter()
+            .map(|line| match last {
+                None => line[data.start_index..].to_string(),
+                Some(val) => line[data.start_index..val].to_string(),
+            })
+            .collect();
 
-    assert!(x.iter().all(|x| x.len() == y.len()));
+        data.operands = parse_rtl_columns(&slice);
 
+        last = Some(data.start_index);
+    }
     let mut sum = 0;
-    for (op, args) in std::iter::zip(y, zip::<&str>(&x)) {
-        let res = parse_rtl_columns(args)
-            .into_iter()
-            .fold(op.default, |acc: u64, val| (op.func)(acc, val));
+    for s in segments {
+        let res = s
+            .operands
+            .iter()
+            .fold(s.operation.default, |acc: u64, val| {
+                (s.operation.func)(acc, *val)
+            });
         sum += res;
     }
     Ok(sum)
 }
 
-fn parse_rtl_columns(string: Vec<&&str>) -> Vec<u64> {
+fn parse_rtl_columns(string: &Vec<String>) -> Vec<u64> {
     if string.is_empty() {
         return vec![];
     }
 
     let max_len = string.iter().map(|s| s.len()).max().unwrap();
-    // println!("Parsing: {string:?} with maxlen {max_len}");
+    // assert!(string.iter().all(|s| s.len() == max_len), "{:?}", string);
+
     (0..max_len)
         .rev()
         .map(|i| {
@@ -119,7 +167,8 @@ fn parse_rtl_columns(string: Vec<&&str>) -> Vec<u64> {
                 .filter_map(|s| {
                     if i < s.len() {
                         // println!("{i}: {}", s[i..=i].to_string());
-                        Some(s[i..=i].to_string())
+                        let val = s[i..=i].to_string().trim().to_string();
+                        return if val.is_empty() { None } else { Some(val) };
                     } else {
                         None
                     }
@@ -127,8 +176,9 @@ fn parse_rtl_columns(string: Vec<&&str>) -> Vec<u64> {
                 .collect::<Vec<String>>()
                 .join("")
         })
+        .filter(|s| !s.is_empty())
         .map(|s| {
-            println!("{s}, {}", s.parse::<u64>().unwrap());
+            // println!("{s}, {}", s.parse::<u64>().unwrap());
             s
         })
         .map(|s| s.parse::<u64>().unwrap())
